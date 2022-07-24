01922982838c9f32785c63a56d6a8c3f8785d3e7
ForLunch01 <crazylunch2019@gmail.com>
2022-07-24 16:29:53 +0900

    finished section 19

--- app.js
+++ app.js
@@ -1,50 +1,56 @@
-const fs =require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended:false }));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/restaurants', function(req, res) {
-    res.render('restaurants')
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 
-app.get('/', function(req, res) {
-    res.render('index')
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
-app.get('/recommend', function(req, res) {
-    res.render('recommend')
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
 });
 
-app.post('/recommend', function(req, res) {
-    const restaurant = req.body;
-    const filePath = path.join(__dirname, 'data', 'restaurants.json');
-    
-    const fileData = fs.readFileSync(filePath);
-    const storedRestaurants = JSON.parse(fileData);
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
 
-    storedRestaurants.push(restaurant);
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
 
-    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  storedRestaurants.push(restaurant);
 
-    res.redirect('/confirm');
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
-app.get('/confirm', function(req, res) {
-    res.render('confirm')
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
 });
 
-app.get('/about', function(req, res) {
-    res.render('about')
+app.get("/about", function (req, res) {
+  res.render("about");
 });
-
-
 
 app.listen(3000);
